ac79cd3f380f59912cac9935cf2a5ee04fd5504f
Andre335 <andre335@gmail.com>
2015-04-23 02:10:03 -0300

    corrigindo bug 2

--- dinamica/logica.js
+++ dinamica/logica.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$("#adicionar").click(function() {
 		increment++;
 		
-		$("#aprender").append("<li id='item-" + increment + "'>" + $("#texto").val() 
+		$("#aprender").append("<li id='item-" + increment + "'>" + $("#texto").text() 
 			+ "<button id='mover-" + increment + "' class='btn btn-small btn-info'> Mover </button>"
 			+ "<button id='remover-" + increment + "' class='btn btn-small btn-danger'> Remover </button>"
 			+ "</li>");	
