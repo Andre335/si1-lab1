b11e51580857f2b276f8c6ca544d37d8528afdcc
Andre335 <andre335@gmail.com>
2015-04-23 13:12:28 -0300

    logica atualizada

--- dinamica/logica.js
+++ dinamica/logica.js
@@ -1,12 +1,17 @@
-var increment = 0;
-
 $(document).ready(function() {
+	var increment = 0;
 	$("#adicionar").click(function() {
 		increment++;
 		
-		$("#aprender").append("<li id='item-" + increment + "'>" + $("#texto").text() 
+		$("#aprender").append("<li id='item-" + increment + "'>" + $("#texto").val() 
 			+ "<button id='mover-" + increment + "' class='btn btn-small btn-info'> Mover </button>"
-			+ "<button id='remover-" + increment + "' class='btn btn-small btn-danger'> Remover </button>"
+			+ "<button id='remover' name='remover-" + increment + "' class='btn btn-small btn-danger'> Remover </button>"
 			+ "</li>");	
+	});
+
+	$("#remover").click(function() {
+
+		alert($this);
+
 	});
 });
